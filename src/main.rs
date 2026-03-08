8e702fa65446061fb6654bdab4f76daf44c8f3e2
Deivid Gabriel <deividgabrielproc@gmail.com>
2026-03-07 22:47:33 -0300

    feat: Added metrics written in CSV

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{
-    fs::{self, File},
-    io::{Cursor, Result, Write},
-    collections::{HashMap, HashSet},
-    cmp::min,
-};
 use arcode::{ArithmeticDecoder, ArithmeticEncoder, EOFKind, Model};
 use bitbit::{BitReader, BitWriter, MSB};
+use std::{
+    cmp::min,
+    collections::{HashMap, HashSet},
+    fs::{self, File},
+    io::{Cursor, Result, Write},
+};
 
 const RHO: u32 = 256;
 const EOF_SYMBOL: u32 = 257;
@@ -38,7 +38,6 @@ struct PPMC {
 }
 
 impl PPMC {
-
     fn new(max_n: usize) -> Self {
         let minus_one = Model::builder()
             .num_symbols(258)
@@ -48,34 +47,47 @@ impl PPMC {
         Self {
             contexts: HashMap::new(),
             minus_one,
-            max_n
+            max_n,
         }
     }
 
-    fn encode(&mut self, data: &[u8], encoder: &mut ArithmeticEncoder, writer: &mut BitWriter<Cursor<Vec<u8>>>) -> Result<()> {
+    fn encode(
+        &mut self,
+        data: &[u8],
+        encoder: &mut ArithmeticEncoder,
+        writer: &mut BitWriter<Cursor<Vec<u8>>>,
+    ) -> Result<()> {
+        let mut metrics_file = File::create(format!("metrics_order_{}.csv", self.max_n))
+            .expect("Erro ao escrever arquivo de métrica");
+        metrics_file.write(b"n,comprimento_medio\n")?;
         // Armazena os N últimos caracteres
         // Ex: N = 3 ['e', 'n', 's']
-        let mut last_characters: Vec<u8> = Vec::with_capacity(self.max_n); 
+        let mut last_characters: Vec<u8> = Vec::with_capacity(self.max_n);
+        let mut total_attributed_bits;
 
+        let mut current_pos = 0;
         for &byte in data {
             let symbol = byte as u32;
             let mut encoded = false;
             // min() é usado para codificar corretamente os N primeiros bytes
             let mut current_n = min(last_characters.len(), self.max_n);
-             
+
             loop {
                 // Extrai a sequência de caracteres a ser observada no contexto atual
                 let context_key = last_characters[last_characters.len() - current_n..].to_vec();
-                
+
                 // Procura a sequência de caracteres no contexto atual, se não
                 // a encontrar, adiciona ao contexto.
                 // Presente: node contem uma referência que aponta para o contexto
                 //           presente dentro do Hash Map
                 // Não Presente: node contêm uma referência que aponta para o novo
                 //               contexto criado
-                let node = self.contexts.entry(context_key.clone()).or_insert_with(Context::new);
-                
-                // Verifica se o símbolo sendo processado já foi visto depois 
+                let node = self
+                    .contexts
+                    .entry(context_key.clone())
+                    .or_insert_with(Context::new);
+
+                // Verifica se o símbolo sendo processado já foi visto depois
                 // do contexto atual
                 if node.seen_symbols.contains(&symbol) {
                     // Se sim, codifica e vai para o próximo símbolo
@@ -86,12 +98,12 @@ impl PPMC {
                     // Se não, codifica o escape e desce de contexto
                     encoder.encode(RHO, &node.model, writer)?;
                 }
-                if current_n == 0{
+                if current_n == 0 {
                     break;
                 }
                 current_n -= 1;
             }
-            
+
             // Se o símbolo nunca foi encontrado, codifica para N = -1
             if !encoded {
                 encoder.encode(symbol, &self.minus_one, writer)?;
@@ -107,22 +119,39 @@ impl PPMC {
                     node.seen_symbols.insert(symbol);
                 }
             }
-            
+
+            // Add metrics to file
+            total_attributed_bits = writer.get_ref().position();
+            metrics_file.write(
+                (format!(
+                    "{},{}\n",
+                    current_pos,
+                    (total_attributed_bits as f64) / current_pos as f64
+                ))
+                .as_bytes(),
+            )?;
+
             // Avança para o próximo símbolo
             last_characters.push(byte);
             if last_characters.len() > self.max_n {
                 last_characters.remove(0);
             }
+
+            current_pos = current_pos + 1;
         }
 
         Ok(())
     }
 
-    fn decode(&mut self, decoder: &mut ArithmeticDecoder, reader: &mut BitReader<Cursor<Vec<u8>>, MSB>) -> Result<Vec<u8>> {
+    fn decode(
+        &mut self,
+        decoder: &mut ArithmeticDecoder,
+        reader: &mut BitReader<Cursor<Vec<u8>>, MSB>,
+    ) -> Result<Vec<u8>> {
         let mut output: Vec<u8> = Vec::new();
         // Armazena os N últimos caracteres
         let mut last_characters: Vec<u8> = Vec::with_capacity(self.max_n);
-        
+
         'decode_loop: loop {
             // min() é usado para decodificar corretamente os N primeiros bytes
             let mut current_n = std::cmp::min(last_characters.len(), self.max_n);
@@ -132,10 +161,13 @@ impl PPMC {
             loop {
                 // Extrai a sequência de caracteres a ser observada no contexto atual
                 let context_key = last_characters[last_characters.len() - current_n..].to_vec();
-                
+
                 // Procura a sequência de caracteres a ser observada no contexto atual,
                 // se não a encontrar, adiciona ao contexto
-                let node = self.contexts.entry(context_key.clone()).or_insert_with(Context::new);
+                let node = self
+                    .contexts
+                    .entry(context_key.clone())
+                    .or_insert_with(Context::new);
 
                 // Lê o stream e decodifica um símbolo baseado no modelo deste contexto
                 let symbol = decoder.decode(&node.model, reader)?;
@@ -156,21 +188,21 @@ impl PPMC {
                     break;
                 }
             }
-            
+
             // Se não foi encontrado antes, decodifica para N = -1
             if !found {
                 decoded_symbol = decoder.decode(&self.minus_one, reader)?;
             }
-            
+
             // Verifica se a compressão acabou
             if decoded_symbol == EOF_SYMBOL {
                 break 'decode_loop;
             }
-        
+
             // Adiciona o byte decodificado a saída final
             let byte = decoded_symbol as u8;
             output.push(byte);
-            
+
             for order in 0..=min(last_characters.len(), self.max_n) {
                 // Define contexto atual
                 let context_key = last_characters[last_characters.len() - order..].to_vec();
@@ -180,22 +212,20 @@ impl PPMC {
                     node.seen_symbols.insert(decoded_symbol);
                 }
             }
-            
+
             // Avança para o próximo símbolo
             last_characters.push(byte);
             if last_characters.len() > self.max_n {
                 last_characters.remove(0);
             }
-            
         }
 
         Ok(output)
     }
 }
 
-
 fn main() {
-    let input_path = "corpus/dickens"; 
+    let input_path = "corpus/dickens";
 
     println!("Lendo arquivo: {}", input_path);
     let sample_bytes = match fs::read(input_path) {
@@ -208,9 +238,12 @@ fn main() {
 
     let original_size = sample_bytes.len();
     println!("Tamanho original: {} bytes\n", original_size);
-    
+
     println!("{:-<60}", "");
-    println!("{:<10} | {:<20} | {:<15}", "Ordem (N)", "Tamanho Comprimido", "Razão (%)");
+    println!(
+        "{:<10} | {:<20} | {:<15}",
+        "Ordem (N)", "Tamanho Comprimido", "Razão (%)"
+    );
     println!("{:-<60}", "");
 
     for n in 1..=10 {
@@ -220,10 +253,14 @@ fn main() {
         let mut encoder = ArithmeticEncoder::new(48);
 
         // Executa a compressão
-        ppmc_encoder.encode(&sample_bytes, &mut encoder, &mut writer).unwrap();
+        ppmc_encoder
+            .encode(&sample_bytes, &mut encoder, &mut writer)
+            .unwrap();
 
         // Finaliza o bitstream
-        encoder.encode(EOF_SYMBOL, &ppmc_encoder.minus_one, &mut writer).unwrap();
+        encoder
+            .encode(EOF_SYMBOL, &ppmc_encoder.minus_one, &mut writer)
+            .unwrap();
         encoder.finish_encode(&mut writer).unwrap();
         writer.pad_to_byte().unwrap();
 
@@ -234,16 +271,16 @@ fn main() {
 
         println!("{:<10} | {:<20} | {:.2}%", n, compressed_size, ratio);
     }
-    
+
     println!("{:-<60}", "");
 }
 
 /*
 fn main() {
-    let input_path = "corpus/dickens"; 
+    let input_path = "corpus/dickens";
 
     println!("Lendo arquivo: {}", input_path);
-    
+
     // Lê o arquivo inteiro para a memória como Vec<u8>
     let sample_bytes = match fs::read(input_path) {
         Ok(bytes) => bytes,
@@ -280,8 +317,8 @@ fn main() {
 
     // Setup do decoder
     println!("Descomprimindo...");
-    let mut ppmc_decoder = PPMC::new(5); 
-    let cursor = Cursor::new(compressed_bytes.clone()); 
+    let mut ppmc_decoder = PPMC::new(5);
+    let cursor = Cursor::new(compressed_bytes.clone());
     let mut reader = BitReader::<_, MSB>::new(cursor);
     let mut decoder = ArithmeticDecoder::new(48);
 
@@ -293,7 +330,7 @@ fn main() {
         Ok(_) => println!("Descomprimido com sucesso!"),
         Err(_) => println!("Erro ao salvar arquivo descomprimido"),
     }
-    
+
     // Verificação de integridade
     if sample_bytes == decompressed_bytes {
         println!("Os bytes descomprimidos são idênticos aos originais!");
